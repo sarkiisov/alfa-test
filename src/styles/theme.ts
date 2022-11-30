321fd6969d285c9094b472fe1eae9bddd5806390
sarkisovTu <sarkisov1337@gmail.com>
2022-11-30 21:53:01 +0300

    fix: theme constants & remove unused file

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -2,7 +2,7 @@ export const theme = {
   colors: {
     white: '#FFFFFF',
     black: '#000000',
-    red: '#E24343',
+    red: '#D42828',
     grey200: '#D6D6D6',
     grey500: '#848484',
     grey800: '#494949',
@@ -10,7 +10,7 @@ export const theme = {
   fontSizes: {
     small: '14px',
     medium: '16px',
-    large: '20px',
+    large: '24px',
   },
   durations: {
     ms200: 200
