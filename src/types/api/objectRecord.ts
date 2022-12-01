631e28f873ce4ade2f66468cb13a0f0ecb372d1b
sarkisovTu <sarkisov1337@gmail.com>
2022-12-01 15:32:00 +0300

    fix: ObjectRecord change interface to type

--- src/types/api/objectRecord.ts
+++ src/types/api/objectRecord.ts
@@ -1,7 +1,7 @@
-export interface ObjectRecord {
+export type ObjectRecord = {
   objectID: number;
   title: string;
   primaryImage: string;
   artistDisplayName: string;
   artistDisplayBio: string;
-}
+};
