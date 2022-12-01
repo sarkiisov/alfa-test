93852240d1a1de971c3fcba560915a5b21ebe971
sarkisovTu <sarkisov1337@gmail.com>
2022-12-01 14:07:39 +0300

    refactor: Typography style props destructuring

--- src/components/Typography/Typograpghy.styled.ts
+++ src/components/Typography/Typograpghy.styled.ts
@@ -1,27 +1,27 @@
 import styled, { css } from 'styled-components';
 import { TypographyProps } from './Typography';
 
-export type DynamicComponentProps = Pick<TypographyProps, 'variant'>;
-
-export const DynamicComponent = styled.div<DynamicComponentProps>`
-  ${(props) => props.variant === 'h2' && css`
-    font-size: ${props.theme.fontSizes.large};
+export const DynamicComponent = styled.div<
+Pick<TypographyProps, 'variant'>
+>`
+  ${({ theme, variant }) => variant === 'h2' && css`
+    font-size: ${theme.fontSizes.large};
     font-weight: 700;
-    color ${props.theme.colors.black};
+    color ${theme.colors.black};
   `}
-  ${(props) => props.variant === 'h3' && css`
-    font-size: ${props.theme.fontSizes.medium};
+  ${({ theme, variant }) => variant === 'h3' && css`
+    font-size: ${theme.fontSizes.medium};
     font-weight: 700;
-    color ${props.theme.colors.black};
+    color ${theme.colors.black};
   `}
-  ${(props) => props.variant === 'body1' && css`
-    font-size: ${props.theme.fontSizes.medium};
+  ${({ theme, variant }) => variant === 'body1' && css`
+    font-size: ${theme.fontSizes.medium};
     font-weight: 400;
-    color ${props.theme.colors.grey800};
+    color ${theme.colors.grey800};
   `}
-  ${(props) => props.variant === 'body2' && css`
-    font-size: ${props.theme.fontSizes.small};
+  ${({ theme, variant }) => variant === 'body2' && css`
+    font-size: ${theme.fontSizes.small};
     font-weight: 400;
-    color ${props.theme.colors.grey800};
+    color ${theme.colors.grey800};
   `}
 `;
