fd7483d488dfdf5a6747632de91b6b82573f29fa
sarkisovTu <sarkisov1337@gmail.com>
2022-12-03 21:15:37 +0300

    feat: filtered objects selector

--- src/store/filter/selectors.ts
+++ src/store/filter/selectors.ts
@@ -2,6 +2,6 @@ import { FilterOptions, FilterState } from '../../types';
 import { RootState } from '../configureStore';
 
 export const filterSelectors = {
-  getFilter: (state: RootState): FilterOptions => state.filter.filter,
+  getFilterOption: (state: RootState): FilterOptions => state.filter.filter,
   getProp: (propKey: keyof FilterState) => (state: RootState) => state.filter[propKey]
 };
--- src/store/objects/selectors.ts
+++ src/store/objects/selectors.ts
@@ -1,8 +1,20 @@
-import { ObjectsState } from '../../types';
+import { ObjectRecord } from '../../types/store/objects';
+import { FilterOptions, ObjectsState } from '../../types';
 import { RootState } from '../configureStore';
 
 export const objectsSelectors = {
   getObjectsCount: (state: RootState): number => state.objects.ids.length,
-  getLoadedObjectsCount: (state: RootState): number => state.objects.objects.length,
+  getLoadedObjects: (state: RootState): ObjectRecord[] => state.objects.objects,
+  getFilteredLoadedObjects: (state: RootState): ObjectRecord[] => {
+    const currentFilter = state.filter.filter;
+    switch (currentFilter) {
+      case FilterOptions.SHOW_ALL:
+        return state.objects.objects;
+      case FilterOptions.SHOW_LIKED:
+        return state.objects.objects.filter((objectProps) => objectProps.isLiked);
+      default:
+        return state.objects.objects;
+    }
+  },
   getProp: (propKey: keyof ObjectsState) => (state: RootState) => state.objects[propKey]
 };
